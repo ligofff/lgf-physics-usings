3a41b1906ec6917a7cb749b9b4e025506568bb00
ligofff <liga@ligofff.ru>
2023-11-15 07:09:40 +0300

    Initial

---
+++ Runtime/PhysicsUsing.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class PhysicsUsing : IDisposable
+{
+    private readonly bool _hitTriggersTempSetting;
+
+    public static PhysicsUsing EnableQueryHitTriggers()
+    {
+        return new PhysicsUsing(true);
+    }
+        
+    public static PhysicsUsing DisableQueryHitTriggers()
+    {
+        return new PhysicsUsing(false);
+    }
+        
+    public static PhysicsUsing QueryHitTriggers(bool state)
+    {
+        return new PhysicsUsing(state);
+    }
+        
+    public PhysicsUsing(bool state)
+    {
+        _hitTriggersTempSetting = Physics2D.queriesHitTriggers;
+        Physics2D.queriesHitTriggers = state;
+    }
+        
+    public void Dispose()
+    {
+        Physics2D.queriesHitTriggers = _hitTriggersTempSetting;
+    }
+}
